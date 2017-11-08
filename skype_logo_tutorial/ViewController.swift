916acdd2048ded7d76e9c9ad02d6d922d81b6dd7
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-11-08 19:13:02 +0900

    init project

---
+++ skype_logo_tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skype_logo_tutorial
+//
+//  Created by Shoichi Kanzaki on 2017/11/08.
+//  Copyright © 2017年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
